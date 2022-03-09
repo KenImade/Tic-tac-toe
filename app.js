44fb08f18be954709a1910949c47604f5e2d1be2
Kenneth Imade <kenneth.imade@yahoo.com>
2022-03-09 14:52:53 +0100

    setup gameboard module

---
+++ app.js
@@ -0,0 +1,42 @@
+// Need a the following
+// a gameboard - a module
+// display - a module
+// players - factory
+
+const gameBoard = (() => {
+
+    let board = ["","","","","","","","",""];
+
+    const getBoardPosition = (input) => {
+        let value = parseInt(input)
+        return value - 1;
+    }
+
+    const updateBoard = (input) => {
+        let playerChoice = parseInt(input);
+
+        if (checkPosition(playerChoice)) {
+            let pos = getBoardPosition(playerChoice);
+            board[pos] = marker;
+        }
+    }
+
+    const checkPosition = (num) => {
+        let pos = getBoardPosition(num);
+
+        if (board[pos] === "") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const getBoard = () => {
+        return board;
+    }
+
+    return {
+        updateBoard,
+        getBoard,
+    }
+})();

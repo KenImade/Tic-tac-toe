9b3bc0b5cec1e4c5eba0bc54928fa0d290b3f860
Kenneth Imade <kenneth.imade@yahoo.com>
2022-03-11 18:24:46 +0100

    updated display controller and created game controller

--- app.js
+++ app.js
@@ -43,11 +43,12 @@ const Player = (marker) => {
 
 const displayController = (() => {
     const fieldElements = document.querySelectorAll(".field");
-    const restartBtn = document.getElementsByClassName("restart-btn");
+    const restartBtn = document.getElementById("restart-btn");
+    const messageBox = document.getElementById("message");
 
     fieldElements.forEach((field) => {
         field.addEventListener("click", (e) => {
-            if (gameController.isOver() || e.target.textContent !== "") return;
+            if (gameController.getGameOver() || e.target.textContent !== "") return;
             gameController.playRound(parseInt(e.target.dataset.index));
             updateGameBoard();
         })
@@ -57,6 +58,7 @@ const displayController = (() => {
         Gameboard.resetBoard();
         gameController.reset();
         updateGameBoard();
+        setMessageElement("X's turn");
     });
 
     const updateGameBoard = () => {
@@ -64,4 +66,80 @@ const displayController = (() => {
             fieldElements[i].textContent = Gameboard.getField(i);
         }
     };
+
+    const setResultMessage = (winner) => {
+        if (winner === "Draw") {
+            setMessageElement("It's a draw!");
+        } else {
+            setMessageElement(`Player ${winner} has won!`);
+        }
+    };
+
+    const setMessageElement = (message) => {
+        messageBox.textContent = message;
+    };
+
+    return {setResultMessage, setMessageElement};
+})();
+
+const gameController = (() => {
+    const playerOne = Player("X");
+    const playerTwo = Player("O");
+    let round = 1;
+    let gameOver = false;
+    
+    const playRound = (position) => {
+        Gameboard.placeMarker(position, getCurrentPlayerMarker());
+        if (checkWinner(position)) {
+            displayController.setResultMessage(getCurrentPlayerMarker());
+            gameOver = true;
+            return;
+        }
+
+        if (round === 9) {
+            displayController.setResultMessage("Draw");
+            gameOver = true;
+            return;
+        }
+        round++;
+        displayController.setMessageElement(
+            `Player ${getCurrentPlayerMarker()}'s turn`
+        );
+    };
+
+    const getCurrentPlayerMarker = () => {
+        return round % 2 === 1 ? playerOne.getMarker() : playerTwo.getMarker();
+    };
+
+    const checkWinner = (position) => {
+        const winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+
+        return winConditions
+            .filter((combination) => combination.includes(position))
+            .some((possibleCombination) =>
+            possibleCombination.every(
+                (index) => Gameboard.getField(index) === getCurrentPlayerMarker()
+            )
+            );
+    };
+
+    const getGameOver = () => {
+        return gameOver;
+    };
+
+    const reset = () => {
+        round = 1;
+        gameOver = false;
+    };
+
+    return {playRound, getGameOver, reset};
 })();
